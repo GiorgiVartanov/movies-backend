edb4e9785db2cc55793b89104111624aae72a2be
PhYell <94902895+PhYell@users.noreply.github.com>
2023-05-05 19:05:10 +0400

    fixed cord

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 const corsOptions = {
-  // origin: "https://movies-spp.netlify.app",
-  origin: "http://localhost:3000",
+  origin: "https://movies-spp.netlify.app",
+  // origin: "http://localhost:3000",
   credentials: true,
   optionSuccessStatus: 200,
 }
